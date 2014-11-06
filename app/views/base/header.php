48bbc8ce00c291c861089e753974ac44171729ff
Robert de Vries <r.devries@robbytu.net>
2014-11-07 00:10:20 +0100

    + Base views and styling

---
+++ app/views/base/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <title><?=$this->data['title'];?> - eatIT</title>
+
+    <link rel="stylesheet" href="/css/default.css" type="text/css" />
+</head>
+<body>
+    <header>
+        <a href="/"></a>
+    </header>
+
+    <div class="content">
