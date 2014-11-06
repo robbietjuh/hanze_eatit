3d1586983cc572a709ae8189b4cd9eba0e02d0f4
Robert de Vries <r.devries@robbytu.net>
2014-11-06 22:56:50 +0100

    + Implemented application config

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * index.php
+ * EatIT
+ *
+ * Main entry point of the application
+ *
+ * @author     R. de Vries <r.devries@robbytu.net>
+ * @version    1.0.0
+ * @date       01/10/14
+ * @copyright  2014 RobbytuProjects
+ * @license    MIT License
+ */
+
+require_once "app/app.php";
+new app;
