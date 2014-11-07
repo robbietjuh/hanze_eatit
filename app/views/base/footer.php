db2ba2e03189eb6a27fcf35cb62c67c4dd2171b8
Robert de Vries <r.devries@robbytu.net>
2014-11-07 01:10:27 +0100

    + Implemented a simple footer

--- app/views/base/footer.php
+++ app/views/base/footer.php
@@ -1,4 +1,20 @@
-
     </div>
+
+    <a href="/basket" class="basket">Naar uw winkelmandje →</a>
+
+    <footer>
+        <div class="holder">
+            <div class="copyright">
+                Copyright © eatIT 2014<br />
+                Alle rechten voorbehouden.<br /><br />
+
+                Onderdeel van een <a href="http://www.hanze.nl">Hanzehogeschool</a> project.
+            </div>
+
+            <div class="links">
+                <a href="/admin">Administratie</a>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
