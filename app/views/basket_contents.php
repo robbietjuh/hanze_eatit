7d6df0bc0ba04f3f0a70c69774fa6fa7daa12108
Robert de Vries <r.devries@robbytu.net>
2014-11-07 01:34:39 +0100

    + Show message in basket view

--- app/views/basket_contents.php
+++ app/views/basket_contents.php
@@ -1,7 +1,8 @@
         <h1>U heeft <?=$this->data['amount'];?> items in uw winkelmandje</h1>
+        <p><?=$this->data['message'];?></p>
 
         <?php foreach($this->data['basket'] as $item) { ?>
-        Item...
+        <?=var_dump($item);?>
         <?php } ?>
 
         <br clear="all" />
