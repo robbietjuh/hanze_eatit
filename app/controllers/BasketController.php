e7d31002de045bded2a90d040fd260408cdc3729
Robert de Vries <r.devries@robbytu.net>
2014-11-07 01:30:25 +0100

    + Implemented the BasketController with basic basket listing renderer

---
+++ app/controllers/BasketController.php
@@ -0,0 +1,29 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * BasketController.php
+ * EatIT
+ *
+ * Controller that will handle requests made to the homepage urldef
+ *
+ * @author     R. de Vries <r.devries@robbytu.net>
+ * @version    1.0.0
+ * @date       01/10/14
+ * @copyright  2014 RobbytuProjects
+ * @license    MIT License
+ */
+
+class BasketController extends MvcBaseController {
+    public function renderBasket($args) {
+        $basket = $this->loadModel("BasketModel");
+
+        $this->data['title'] = 'Winkelmandje';
+        $this->data['basket'] = $basket->getContents();
+
+        $this->renderView("base/header");
+        $this->renderView("basket_contents");
+        $this->renderView("base/footer");
+    }
+}
+
